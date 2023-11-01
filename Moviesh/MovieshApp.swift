12f090b964ac2aa691e74d7f983fb69866a72c29
Filip Miladinovic <filip982@gmail.com>
2023-11-01 18:41:46 +0100

    Initial Commit

---
+++ Moviesh/MovieshApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieshApp.swift
+//  Moviesh
+//
+//  Created by Filip Miladinovic on 01.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MovieshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
